4ccc79bc8158bb306a6bdf84dc265beebb95861d
[dnqls2446] <[dnqls9875@gmail.com]>
2023-03-23 17:33:35 +0900

    커밋

--- momentum/event/app.js
+++ momentum/event/app.js
@@ -1,17 +1,7 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
-function handleTitleClick() {
-  title.style.color = "blue";
+function handleh1Click() {
+  h1.style.color = "blue";
 }
 
-function handleMouseEnter() {
-  title.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-  title.innerText = "Mouse is gone!";
-}
-
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+h1.addEventListener("click", handleh1Click);
